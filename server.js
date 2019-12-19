880bbd7217dfa977ed5b31987cd3ad5238e19730
Denver <dnaicker@gmail.com>
2019-12-19 12:41:46 +0200

    readme update and install update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
+const open = require("open");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -24,24 +25,14 @@ const db = new sqlite3.Database(dbFile);
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(() => {
   if (!exists) {
-    
+
     db.run(
       "CREATE TABLE Calculations (id INTEGER PRIMARY KEY AUTOINCREMENT, calculations TEXT)"
     );
-    console.log("New table Dreams created!");
-
-    // insert default dreams
-    // db.serialize(() => {
-    //   db.run(
-    //     'INSERT INTO Calculations (calculation) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
-    //   );
-    // });
   } else {
     console.log('Database "Calculations" ready to go!');
     db.each("SELECT * from Calculations", (err, row) => {
-      if (row) {
-        console.log(row);
-      }
+
     });
   }
 });
@@ -51,16 +42,13 @@ app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
-// endpoint to get all the dreams in the database
 app.get("/getCalculations", (request, response) => {
   db.all("SELECT * from Calculations", (err, rows) => {
     response.send(JSON.stringify(rows));
   });
 });
 
-// endpoint to add a dream to the database
 app.post("/addCalculations", (request, response) => {
-  console.log(request.body.calculations);
 
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
   if (!process.env.DISALLOW_WRITE) {
@@ -78,11 +66,12 @@ app.post("/addCalculations", (request, response) => {
 });
 
 // helper function that prevents html/css/script malice
-const cleanseString = function(string) {
+const cleanseString = function (string) {
   return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 };
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
+  console.log(`Your app is listening on port http://localhost:${listener.address().port}`);
+  open('http://localhost:' + listener.address().port)
 });
